8e4cfa55d246438d2f5efb292072eee7baad3693
akram <akramgaltam@yahoo.fr>
2024-05-12 23:32:43 +0200

    Commit du Front

--- front-skeleton/src/app/app.component.ts
+++ front-skeleton/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from "@angular/core"
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: "root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = "front-skeleton"
+export class AppComponent implements OnInit{
+  title = 'Tracolab';
+
+  constructor() {
+  }
+  ngOnInit() {
+  }
+
 }
--- front-skeleton/src/app/app.module.ts
+++ front-skeleton/src/app/app.module.ts
@@ -1,43 +1,48 @@
-import { NgModule } from "@angular/core"
-import { BrowserModule } from "@angular/platform-browser"
-
-import { AppRoutingModule } from "app-routing.module"
-import { AppComponent } from "app.component"
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
-import { NavbarComponent } from "navbar/navbar.component"
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http"
+import { CommonModule } from "@angular/common";
 import { MatListModule } from "@angular/material/list"
-import { HomeComponent } from "home/home.component"
-import { StudentsComponent } from "students/students.component"
-import { StudentDetailsComponent } from "students/student-details/student-details.component"
-import { FormsModule } from "@angular/forms"
 import { MatIconModule } from "@angular/material/icon"
 import { MatButtonModule } from "@angular/material/button"
-import { MajorsComponent } from "majors/majors.component"
-import { MajorStudentsComponent } from "majors/major-students/major-students.component"
-import { HttpClientModule } from "@angular/common/http"
+import { MatCardModule } from '@angular/material/card';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { ListeComponent } from './liste/liste.component';
+import { FooterComponent } from './footer/footer.component';
+import { ArticleComponent } from './article/article.component';
+
+import { ArticleService } from './services/articles.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     HomeComponent,
-    StudentsComponent,
-    StudentDetailsComponent,
-    MajorsComponent,
-    MajorStudentsComponent,
+    HeaderComponent,
+    BannerComponent,
+    ListeComponent,
+    FooterComponent,
+    ArticleComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
-    MatListModule,
+    AppRoutingModule,
     FormsModule,
+    HttpClientModule,
+    CommonModule,
+    MatListModule,
     MatIconModule,
     MatButtonModule,
-    HttpClientModule,
+    MatCardModule
   ],
-  providers: [],
-  bootstrap: [AppComponent],
+  providers: [ArticleService],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
